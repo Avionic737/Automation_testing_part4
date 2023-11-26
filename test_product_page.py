ca0b22a9ef902971e5c1c0535605d02e10b98d19
Avion737 <JuryG@mtnhausdigital.com>
2023-11-26 11:47:40 -0500

    added tests for registered users

--- test_product_page.py
+++ test_product_page.py
@@ -1,7 +1,10 @@
 import pytest
-from .pages.product_page import ProductPage
 from .pages.basket_page import BasketPage
+from .pages.locators import ProductPageLocators
+from .pages.product_page import ProductPage
+import time
 
+link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
 
 '''
 @pytest.mark.parametrize('promo_offer', ["0", "1", "2", "3", "4", "5", "6",
@@ -33,9 +36,7 @@ def test_guest_can_go_to_login_page_from_product_page(browser):
     page.go_to_login_page()
 '''
 
-
 def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
     page.open()
     page.go_to_basket()
@@ -44,5 +45,45 @@ def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     basket_page.should_be_empty_basket()
     basket_page.should_be_empty_basket_message()
     basket_page.should_not_be_items_in_basket()
+
+
+def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
+    page = ProductPage(browser, link)
+    page.open()
+    page.add_to_basket()
+    assert page.is_not_element_present(*ProductPageLocators.ADDED_TO_BASKET_MESSAGE), \
+        "Success message is present, but it should not be"
+
+
+def test_guest_cant_see_success_message(browser):
+    page = ProductPage(browser, link)
+    page.open()
+    assert page.is_not_element_present(*ProductPageLocators.ADDED_TO_BASKET_MESSAGE), \
+        "Success message is present, but it should not be"
+
+
+def test_message_disappeared_after_adding_product_to_basket(browser):
+    page = ProductPage(browser, link)
+    page.open()
+    page.add_to_basket()
+    assert page.is_disappeared(*ProductPageLocators.ADDED_TO_BASKET_MESSAGE), \
+        "Success message did not disappear, but it should have"
+
+
+class TestUserAddToBasketFromProductPage:
+    def test_user_cant_see_success_message(self, browser):
+        page = ProductPage(browser, link)
+        page.open()
+        assert page.is_not_element_present(*ProductPageLocators.ADDED_TO_BASKET_MESSAGE), \
+            "Success message is present, but it should not be"
+
+    def test_user_can_add_product_to_basket(self, browser):
+        page = ProductPage(browser, link)
+        page.open()
+        product_name, product_price = page.get_book_info()
+        page.add_to_basket()
+        page.solve_quiz_and_get_code()
+        page.should_be_added_to_basket_message(product_name)
+        page.should_be_basket_total_price(product_price)
 
 # pytest -s test_product_page.py
